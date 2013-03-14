44aed3a7ce00a08d4820d309dfb11bb3fd23a563
Tyrre Burks <tburks@teaminterval.com>
2013-03-14 02:32:11 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamInterval::Application.config.secret_token = 'b35570c40891f39b7d29b35a520f46d63608e55f5cab48d1327260fb4331c8b2d998850c07603fe78643b4cd8e34650373ef24b9fec9d6463ccbb85af9baa260'
